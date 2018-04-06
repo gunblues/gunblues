cecf4ae498929afe407c44eb3cf39aad9c6c1182
kevin <hsiao.powen@gmail.com>
2018-04-06 11:56:16 +0800

    langja -> mvbowbow

--- web/app/application/locale/tw/trans.php
+++ web/app/application/locale/tw/trans.php
@@ -6,15 +6,13 @@ define("HTML_KEYWORDS", "短片爆報,愛比價,便宜的台股,無料音樂");
 define("HTML_DESCRIPTION", "短片爆報,愛比價,便宜的台股,無料音樂");
 
 define("MYYASUI", "愛比價");
-define("URMUISC", "無料音樂");
+define("MVBOWBOW", "MV爆報");
 define("URFILM", "短片爆報");
 define("TWSTOCK", "便宜的台股");
-define("ILOVEPOPCORN", "我愛爆爆");
 
 define("MYYASUI_DESC", "整合各大購物網站，輕鬆比價");
-define("URMUISC_DESC", "整合Youtube，維基和排行榜，掌握最新音樂脈動");
+define("MVBOWBOW_DESC", "提供給你最具人氣的MV，還有各類型歌手的作品集及MV");
 define("URFILM_DESC", "簡單易用的介面讓你輕鬆看 Youtube 影片");
 define("TWSTOCK_DESC", "提供總體經濟指標和台股相關的數據");
-define("ILOVEPOPCORN_DESC", "未婚聯誼活動,單身聯誼交友");
 
 // vim: expandtab softtabstop=4 tabstop=4 shiftwidth=4 ts=4 sw=4
--- web/app/application/locale/us/trans.php
+++ web/app/application/locale/us/trans.php
@@ -6,15 +6,13 @@ define("HTML_KEYWORDS", "popFilm,IBJ,Taiwan Stock,freeMusic");
 define("HTML_DESCRIPTION", "popFilm,IBJ,Taiwan Stock,freeMusic");
 
 define("MYYASUI", "IBJ");
-define("URMUISC", "Langja");
+define("MVBOWBOW", "MvBowBow");
 define("URFILM", "VideoBowBow");
 define("TWSTOCK", "Taiwan Stock");
-define("ILOVEPOPCORN", "我愛爆爆");
 
 define("MYYASUI_DESC", "Integrate shopping sites in Taiwan to let you find the goods is affordable but of high quality");
-define("URMUISC_DESC", "Integrate youtube, wikipedia and billboard");
+define("URMUISC_DESC", "Watch the most popular music video and different kind of singers' collections ");
 define("URFILM_DESC", "Easy to watch videos on Youtube");
 define("TWSTOCK_DESC", "Economic indicators and Taiwan Stock");
-define("ILOVEPOPCORN_DESC", "未婚聯誼活動,單身聯誼交友");
 
 // vim: expandtab softtabstop=4 tabstop=4 shiftwidth=4 ts=4 sw=4
